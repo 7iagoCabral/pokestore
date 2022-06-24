86cd5ec2a653c207a248c1012522e2b79618f3dc
Tiago Cabral <tiagocabraldafonseca@gmail.com>
2022-06-24 02:36:56 -0300

    :sparkles: feat: implement context and local storage

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const MyCartStorage = JSON.parse(localStorage.getItem('mypokedex')) || []
+
+export const CartContext = React.createContext(MyCartStorage)
+export const CartContextProvide = props => {
+
+    function PushMyCart ({...pokemon}){
+        let pokemonsByName = MyCartStorage.map(pokemon => pokemon.name)
+        let onMyCart = pokemonsByName.some(el => el == pokemon.name)
+        if(!onMyCart){
+
+            MyCartStorage.push(
+               
+                {
+                    name: pokemon.name,
+                    avatarImg:pokemon.avatarImg,
+                    infos: pokemon.infos, 
+                    qtd: 1,
+                    price: pokemon.price
+                }
+            )
+
+            return localStorage.setItem('mypokedex', JSON.stringify(MyCartStorage)   )
+        }
+
+
+    }
+    
+    
+    function handleBuyPokemon({...pokemon}){
+        PushMyCart({...pokemon})
+    }
+
+    function handleRemovePokemon({...pokemon}){
+        
+        
+    }
+    function handleIncrementPokemon({...pokemon}){
+
+    }
+    function handleDecrementPokemon({...pokemon}){
+
+    }
+
+
+    return(
+        <CartContext.Provider value={{
+            handleBuyPokemon, 
+            MyCartStorage,
+        }}>
+            {props.children}
+        </CartContext.Provider>
+        
+    );
+}
